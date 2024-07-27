d0776dcb1c6ecf04cebf1e4df9aeeb87d60daa6c
Aswinn3 <aswinselvan3@gmail.com>
2024-07-27 21:47:21 +0530

    updated snake

--- src/components/Snake.tsx
+++ src/components/Snake.tsx
@@ -12,7 +12,15 @@ interface SnakeProps{
 
 export default function Snake( { snake }: SnakeProps): JSX.Element{
     return(
-        <></>
+        <Fragment>
+            {snake.map((segment: any, index: number) =>{
+                const segmentStyle = {
+                    left: segment.x * 10,
+                    top: segment.y * 10,
+                };
+                return <View key={index} style={[styles.snake, segmentStyle]}/>;
+            })}
+        </Fragment>
     );
 }
 
