eb79ad21f49c22a5dd20400c357bca723717341f
Aswinn3 <aswinselvan3@gmail.com>
2024-07-27 21:58:05 +0530

    updated header

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { StyleSheet, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "../styles/colors";
+import { FontAwesome } from "@expo/vector-icons";
+
+
