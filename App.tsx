07fc905a3ab472777bdec1ea053ec67a127354ab
Aswinn3 <aswinselvan3@gmail.com>
2024-07-28 09:28:16 +0530

    updated app file

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import Game from "./src/components/Game";
 import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-const App = () => (
+const App = (): JSX.Element => (
   <GestureHandlerRootView style={{ flex: 1 }}>
     <Game />
   </GestureHandlerRootView>
