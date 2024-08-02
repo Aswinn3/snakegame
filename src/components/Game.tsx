6231b778f5d588e8bbf7b900921fd0bf8a3735a3
Aswinn3 <aswinselvan3@gmail.com>
2024-08-02 13:07:10 +0530

    updated game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,32 @@
-import * as React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { Colors } from '../styles/colors';
+import { useEffect, useState } from "react";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import { Colors } from "../styles/colors";
+import { Direction, Coordinate, GestureEventType } from "../types/types";
+import { checkEatsFood } from "../utils/checkEatsFood";
+import { checkGameOver } from "../utils/checkGameOver";
+import { randomFoodPosition } from "../utils/randomFoodPosition";
+import Food from "./Food";
+import Header from "./Header";
+import Score from "./Score";
+import Snake from "./Snake";
+
+
+const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const MOVE_INTERVAL = 50;
+const SCORE_INCREMENT = 10;
+
 
  export default function Game():JSX.Element{
-    
+    const [directoion, setDirection] = useState<Direction>(Direction.Right);
+    const [snake, setSnake] = useState<Coordinate[]>(SNAKE_INITIAL_POSITION);
+    const [food, setFood] = useState<Coordinate>(FOOD_INITIAL_POSITION);
+    const [Score, setScore] = useState<number>(0);
+    const [isGameOver, setIsGameOver] = useState<boolean>(false);
+    const [isPaused, setIsPaused] = useState<boolean>(false);
+
     const handleGesture = () => {
         
     }
